6bd5a641aac20cc115426cc2f6629cc7a993fd4c
André R <ar@ez.no>
2017-10-13 12:24:13 +0200

    TMP: Skeleton for paralell search index refresh support (on top of 6.7)

--- eZ/Bundle/EzPublishCoreBundle/Command/ReindexCommand.php
+++ eZ/Bundle/EzPublishCoreBundle/Command/ReindexCommand.php
@@ -8,11 +8,15 @@
  */
 namespace eZ\Bundle\EzPublishCoreBundle\Command;
 
+use eZ\Publish\Core\Search\Common\IterativelyIndexer;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\PhpExecutableFinder;
 use eZ\Publish\Core\Search\Common\Indexer;
+use ezcSystemInfo;
 use RuntimeException;
 
 class ReindexCommand extends ContainerAwareCommand
@@ -46,12 +50,20 @@ class ReindexCommand extends ContainerAwareCommand
     {
         $this
             ->setName('ezplatform:reindex')
-            ->setDescription('Recreate search engine index')
-            ->addOption('iteration-count', 'c', InputOption::VALUE_OPTIONAL, 'Number of objects to be indexed in a single iteration', 20)
+            ->setDescription('Recreate or Refresh search engine index')
+            ->addOption('iteration-count', 'c', InputOption::VALUE_OPTIONAL, 'Number of objects to be indexed in a single iteration, for avoiding using to much memory', 20)
             ->addOption('no-commit', null, InputOption::VALUE_NONE, 'Do not commit after each iteration')
+            ->addOption('no-purge', null, InputOption::VALUE_NONE, 'Do not purge before indexing. BC NOTE: Should this be default as of 2.0?')
+            ->addOption('since', null, InputOption::VALUE_NONE, 'Index changes since a given time, any format understood by DateTime. Implies "no-purge", can not be combined with "content-ids".')
+            ->addOption('content-ids', null, InputOption::VALUE_NONE, 'Comma separated list of content id\'s to refresh (deleted or updated/added). Implies "no-purge", can not be combined with "since".')
+            ->addOption('processes', null, InputOption::VALUE_NONE, 'Number of sub processes to spawn in parallel, by default: (number of cpu cores)-1, disable by setting to "0"', $this->getNumberOfCPUCores() -1)
             ->setHelp(
                 <<<EOT
 The command <info>%command.name%</info> indexes current configured database in configured search engine index.
+
+
+TODO: ADD EXAMPLES OF ADVANCE USAGE!
+
 EOT
             );
     }
@@ -61,13 +73,81 @@ EOT
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $iterationCount = $input->getOption('iteration-count');
         $noCommit = $input->getOption('no-commit');
-
-        if (!is_numeric($iterationCount) || (int)$iterationCount < 1) {
+        $iterationCount = $input->getOption('iteration-count');
+        if (!is_numeric($iterationCount) || (int) $iterationCount < 1) {
             throw new RuntimeException("'--iteration-count' option should be > 0, got '{$iterationCount}'");
         }
 
-        $this->searchIndexer->createSearchIndex($output, intval($iterationCount), empty($noCommit));
+
+        if ($this->searchIndexer instanceof IterativelyIndexer) {
+            return $this->executeParallel($input, $output, (int) $iterationCount, (bool) $noCommit);
+        }
+
+        $output->writeln( <<<EOT
+DEPRECATED:
+Running indexing against an Indexer that has not been updated to use IterativelyIndexer abstract, options that won't be
+taken into account:
+- since
+- content-ids
+- processes
+- no-purge
+EOT
+        );
+        $this->searchIndexer->createSearchIndex($output, (int) $iterationCount, empty($noCommit));
+    }
+
+
+    private function executeParallel(InputInterface $input, OutputInterface $output, $iterationCount, $noCommit) {
+        $since = $input->getOption('since');
+        $contentIds = $input->getOption('content-ids');
+        $processes = $input->getOption('processes');
+        $noPurge = $input->getOption('no-purge');
+
+
+    }
+
+
+    private static function getPhpProcess($consoleDir = 'app')
+    {
+        $phpFinder = new PhpExecutableFinder();
+        if (!$phpPath = $phpFinder->find()) {
+            throw new \RuntimeException('The php executable could not be found, add it to your PATH environment variable and try again');
+        }
+
+        $cmd = 'ezplatform:reindex';
+        $php = escapeshellarg($phpFinder);
+        $console = escapeshellarg($consoleDir.'/console');
+
+        return new Process($php.' '.$console.' '.$cmd, null, null, null, null);
+    }
+
+    private function getNumberOfCPUCores()
+    {
+        $cores = 1;
+        if (is_file('/proc/cpuinfo')) {
+            // Linux (and potentially Windows with linux sub systems)
+            $cpuinfo = file_get_contents('/proc/cpuinfo');
+            preg_match_all('/^processor/m', $cpuinfo, $matches);
+            $cores = count($matches[0]);
+        } else if (DIRECTORY_SEPARATOR === '\\') {
+            // Windows
+            if (($process = @popen('wmic cpu get NumberOfCores', 'rb')) !== false) {
+                fgets($process);
+                $cores = (int) fgets($process);
+                pclose($process);
+            }
+        } else {
+            // *nix (Linux, BSD and Mac)
+            if (($process = @popen('sysctl -a', 'rb')) !== false) {
+                $output = stream_get_contents($process);
+                if (preg_match('/hw.ncpu: (\d+)/', $output, $matches)) {
+                    $cores = (int) $matches[1][0];
+                }
+                pclose($process);
+            }
+        }
+
+        return $cores;
     }
 }
